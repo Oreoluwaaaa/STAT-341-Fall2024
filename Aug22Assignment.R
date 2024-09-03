fe14444d11ea2b8d831d0f9b61f33aaefc18e5d9
Oreoluwaaaa <97929513+Oreoluwaaaa@users.noreply.github.com>
2024-09-03 16:15:13 -0400

    Create Aug22Assignment.R

---
+++ Aug22Assignment.R
@@ -0,0 +1,13 @@
+#Author: Oreoluwa Ala, Date:08/22/2024, Purpose: Test measures of descriptive stats
+
+# How to calculate standard deviation in R
+X <- c(1,2,3,4,5)
+
+sd(X)
+
+#how to calculate the mean of X in R
+
+mean(X)
+
+#how to calculate the median of X in R
+median(X)
