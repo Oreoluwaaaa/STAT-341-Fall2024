f6bbba0515f65e3b2e50e6585971589929c49fef
Oreoluwaaaa <97929513+Oreoluwaaaa@users.noreply.github.com>
2024-09-12 16:19:21 -0400

    Add files via upload

---
+++ Correlation.R
@@ -0,0 +1,13 @@
+#Author: Oreoluwa; Date: Sept 12, 2024; Purpose: Perform Correlation Analysis
+library("ggpubr")
+
+#loading dummy data for testing
+my_data <- mtcars
+
+#Hypothesis: the weight of the car is inversely proportional to the mpd it travels
+
+res <- cor.test(my_data$wt, my_data$mpg, method="pearson")
+
+#cor 
+#-0.8676594 
+#there is a strong inverse relationship between the car's weight and mpg. The hypothesis is true.
